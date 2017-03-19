9412286daf5e74e4fcc64cf2f7db1e6c00e1680d
wangdahoo <157195705@qq.com>
2017-03-19 17:06:07 +0800

    nodeTree, allNodes and degree

---
+++ list.js
@@ -0,0 +1,24 @@
+var app = document.getElementById('app')
+var avatar = 'https://avatars0.githubusercontent.com/u/8207553?v=3&u=a269f5bb2f0eff34f3853c9e51f1d5cbb549c910&s=100'
+var data = [
+  { nickname: '王大虎', memo: '这家伙很懒，什么也没留下', avatar: avatar },
+  { nickname: '王大虎', memo: '这家伙很懒，什么也没留下', avatar: avatar },
+  { nickname: '王大虎', memo: '这家伙很懒，什么也没留下', avatar: avatar },
+  { nickname: '王大虎', memo: '这家伙很懒，什么也没留下', avatar: avatar },
+  { nickname: '王大虎', memo: '这家伙很懒，什么也没留下', avatar: avatar }
+]
+var items = []
+_.each(data, function (d) {
+  items.push(new E('li', { className: 'item' }, [
+    new E('h2', [d.nickname]),
+    new E('p', [d.memo]),
+    new E('img', { src: d.avatar })
+  ]))
+})
+var ul = new E('ul', { className: 'list' }, items)
+
+app.appendChild(ul.render())
+
+console.log(ul.nodeTree())
+console.log(ul.allNodes())
+console.log(ul.degree())
